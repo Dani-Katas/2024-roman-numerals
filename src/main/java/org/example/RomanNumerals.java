573bb56392c227bdd78ab3709d174907b82506dd
Diego Machín <diego@acidtango.com>
2024-06-14 16:10:41 +0100

    green: 40

--- src/main/java/org/example/RomanNumerals.java
+++ src/main/java/org/example/RomanNumerals.java
@@ -4,19 +4,19 @@ public class RomanNumerals {
 
   public static String toRoman(final int number) {
     if (number == 40) {
-      return "XL";
+      return "X" + "L";
     }
     if (number >= 10) {
       return "X" + toRoman(number - 10);
     }
     if (number == 9) {
-      return "IX";
+      return "I" + "X";
     }
     if (number >= 5) {
       return "V" + toRoman(number - 5);
     }
     if (number == 4) {
-      return "IV";
+      return "I" + "V";
     }
 
     return "I".repeat(number);
