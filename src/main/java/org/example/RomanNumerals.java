9976a0b3a79f97da53b989cb4b69a3ee334a2a1b
Diego Machín <diego@acidtango.com>
2024-06-14 15:54:21 +0100

    green: 13

--- src/main/java/org/example/RomanNumerals.java
+++ src/main/java/org/example/RomanNumerals.java
@@ -3,6 +3,9 @@ package org.example;
 public class RomanNumerals {
 
   public static String toRoman(final int number) {
+    if (number == 13) {
+      return "XIII";
+    }
     if (number == 12) {
       return "XII";
     }
