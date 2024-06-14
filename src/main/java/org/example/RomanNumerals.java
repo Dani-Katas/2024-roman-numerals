1d9b9473987432838d023e0821d83b95391fe5c6
Daniel Ramos <danielramosacosta1@gmail.com>
2024-06-14 16:20:20 +0200

    green: transforms_three_to_roman

--- src/main/java/org/example/RomanNumerals.java
+++ src/main/java/org/example/RomanNumerals.java
@@ -3,6 +3,10 @@ package org.example;
 public class RomanNumerals {
 
   public static String toRoman(final int number) {
+    if (number == 3) {
+      return "III";
+    }
+
     if (number == 2) {
       return "II";
     }
