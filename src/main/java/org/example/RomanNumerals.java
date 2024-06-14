c168fdeddd5db74ff6049c04674378806bd4a870
Daniel Ramos <danielramosacosta1@gmail.com>
2024-06-14 16:23:11 +0200

    green: transforms_four_to_roman

--- src/main/java/org/example/RomanNumerals.java
+++ src/main/java/org/example/RomanNumerals.java
@@ -3,6 +3,10 @@ package org.example;
 public class RomanNumerals {
 
   public static String toRoman(final int number) {
+    if (number == 4) {
+      return "IV";
+    }
+
     return "I".repeat(number);
   }
 }
