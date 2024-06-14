8921b721d281e1dcb5ced05ac744c2bd1767054c
Daniel Ramos <danielramosacosta1@gmail.com>
2024-06-14 16:22:08 +0200

    refactor: use.repeat(number)

--- src/main/java/org/example/RomanNumerals.java
+++ src/main/java/org/example/RomanNumerals.java
@@ -3,14 +3,6 @@ package org.example;
 public class RomanNumerals {
 
   public static String toRoman(final int number) {
-    if (number == 3) {
-      return "III";
-    }
-
-    if (number == 2) {
-      return "II";
-    }
-
-    return "I";
+    return "I".repeat(number);
   }
 }
