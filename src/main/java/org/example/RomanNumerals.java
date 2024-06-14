df25a56d671083d398f20acc093e03f0a26a4426
Daniel Ramos <danielramosacosta1@gmail.com>
2024-06-14 17:18:47 +0200

    refactor: replace hardcoded literals with toRoman()

--- src/main/java/org/example/RomanNumerals.java
+++ src/main/java/org/example/RomanNumerals.java
@@ -3,28 +3,39 @@ package org.example;
 public class RomanNumerals {
 
   public static String toRoman(final int number) {
-    if (number == 51) {
-      return "L" + toRoman(number - 50);
-    }
     if (number == 50) {
       return "L";
     }
-    if (number == 40) {
-      return "X" + "L";
+    if (number == 10) {
+      return "X";
     }
-    if (number >= 10) {
-      return "X" + toRoman(number - 10);
+    if (number == 5) {
+      return "V";
     }
-    if (number == 9) {
-      return "I" + "X";
-    }
-    if (number >= 5) {
-      return "V" + toRoman(number - 5);
-    }
-    if (number == 4) {
-      return "I" + "V";
+    if (number == 1) {
+      return "I";
     }
 
-    return "I".repeat(number);
+    if (number == 51) {
+      return toRoman(50) + toRoman(number - 50);
+    }
+    if (number == 40) {
+      return toRoman(10) + toRoman(number + 10);
+    }
+    if (number >= 11) {
+      return toRoman(10) + toRoman(number - 10);
+    }
+    if (number == 9) {
+      return toRoman(1) + toRoman(number + 1);
+    }
+    if (number >= 6) {
+      return toRoman(5) + toRoman(number - 5);
+    }
+
+    if (number == 4) {
+      return toRoman(1) + toRoman(5);
+    }
+
+    return toRoman(1).repeat(number);
   }
 }
