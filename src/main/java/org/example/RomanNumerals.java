e7b4589d36a63e90d2e4c881a2e97a34d55dec04
Diego Machín <diego@acidtango.com>
2024-06-14 16:01:40 +0100

    refactor: direct transformations

--- src/main/java/org/example/RomanNumerals.java
+++ src/main/java/org/example/RomanNumerals.java
@@ -3,23 +3,15 @@ package org.example;
 public class RomanNumerals {
 
   public static String toRoman(final int number) {
-    if (number >= 11) {
+    if (number >= 10) {
       return "X" + toRoman(number - 10);
-    }
-    if (number == 10) {
-      return "X";
     }
     if (number == 9) {
       return "IX";
     }
-    if (number >= 6) {
+    if (number >= 5) {
       return "V" + toRoman(number - 5);
     }
-
-    if (number == 5) {
-      return "V";
-    }
-
     if (number == 4) {
       return "IV";
     }
