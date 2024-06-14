ff9d0dd1dc1ef5f7284bb609c0d1d1c544f4bad1
Diego Machín <diego@acidtango.com>
2024-06-14 16:11:41 +0100

    green: 50

--- src/main/java/org/example/RomanNumerals.java
+++ src/main/java/org/example/RomanNumerals.java
@@ -3,6 +3,9 @@ package org.example;
 public class RomanNumerals {
 
   public static String toRoman(final int number) {
+    if (number == 50) {
+      return "L";
+    }
     if (number == 40) {
       return "X" + "L";
     }
