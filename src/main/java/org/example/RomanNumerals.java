f3408424ca30cb0a403dc166b35f33fef55a70ae
Daniel Ramos <danielramosacosta1@gmail.com>
2024-06-14 16:46:46 +0200

    refactor: reuse toRoman in other cases

--- src/main/java/org/example/RomanNumerals.java
+++ src/main/java/org/example/RomanNumerals.java
@@ -3,16 +3,8 @@ package org.example;
 public class RomanNumerals {
 
   public static String toRoman(final int number) {
-    if (number == 8) {
-      return "V" + toRoman(3);
-    }
-
-    if (number == 7) {
-      return "V" + toRoman(2);
-    }
-
-    if (number == 6) {
-      return "V" + toRoman(1);
+    if (number >= 6) {
+      return "V" + toRoman(number - 5);
     }
 
     if (number == 5) {
