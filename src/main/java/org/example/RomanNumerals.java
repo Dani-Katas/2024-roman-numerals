cf8b2f9103cf5793edfece2f12060168d7c6e1d4
Diego Machín <diego@acidtango.com>
2024-06-14 15:59:14 +0100

    refactor: greater than 11

--- src/main/java/org/example/RomanNumerals.java
+++ src/main/java/org/example/RomanNumerals.java
@@ -3,14 +3,8 @@ package org.example;
 public class RomanNumerals {
 
   public static String toRoman(final int number) {
-    if (number == 13) {
-      return "XIII";
-    }
-    if (number == 12) {
-      return "XII";
-    }
-    if (number == 11) {
-      return "XI";
+    if (number >= 11) {
+      return "X" + toRoman(number - 10);
     }
     if (number == 10) {
       return "X";
