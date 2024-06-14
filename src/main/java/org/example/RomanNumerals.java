352a4e03a29ef4dc645c90d3a2afc6088c94e707
Daniel Ramos <danielramosacosta1@gmail.com>
2024-06-14 16:19:27 +0200

    green: transforms_two_to_roman

--- src/main/java/org/example/RomanNumerals.java
+++ src/main/java/org/example/RomanNumerals.java
@@ -3,6 +3,10 @@ package org.example;
 public class RomanNumerals {
 
   public static String toRoman(final int number) {
+    if (number == 2) {
+      return "II";
+    }
+
     return "I";
   }
 }
