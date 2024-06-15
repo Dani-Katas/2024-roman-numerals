4f7a114e143049b77b6d07763ac5eb0646a2c878
Daniel Ramos <danielramosacosta1@gmail.com>
2024-06-15 21:26:12 +0200

    green: 5-10

--- src/main/java/org/example/RomanNumerals.java
+++ src/main/java/org/example/RomanNumerals.java
@@ -3,6 +3,26 @@ package org.example;
 public class RomanNumerals {
 
   public static String toRoman(final int number) {
+    if (number == 10) {
+      return "X";
+    }
+
+    if(number == 9) {
+      return "IX";
+    }
+
+    if (number > 5) {
+      return "V" + toRoman(number - 5);
+    }
+
+    if (number == 5) {
+      return "V";
+    }
+
+    if (number == 4) {
+      return "IV";
+    }
+
     return "I".repeat(number);
   }
 }
