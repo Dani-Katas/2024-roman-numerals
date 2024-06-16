f75d0d2a445806a2a08a5393943fe657585d2c34
Daniel Ramos <danielramosacosta1@gmail.com>
2024-06-16 20:01:03 +0200

    remove old method

--- src/main/java/org/example/RomanNumerals.java
+++ src/main/java/org/example/RomanNumerals.java
@@ -24,28 +24,6 @@ public class RomanNumerals {
     return new Roman("", 0);
   }
 
-  private static int getRestableFor(final int i) {
-    if (i == 1000) {
-      return 100;
-    }
-    if (i == 500) {
-      return 100;
-    }
-    if (i == 100) {
-      return 10;
-    }
-    if (i == 50) {
-      return 10;
-    }
-    if (i == 10) {
-      return 1;
-    }
-    if (i == 5) {
-      return 1;
-    }
-    return 0;
-  }
-
   public static String toRoman(final int number) {
     for (Roman roman : Roman.UNITS) {
       final int value = roman.value();
