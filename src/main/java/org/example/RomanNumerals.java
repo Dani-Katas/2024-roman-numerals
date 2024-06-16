81232f79864efe68869a29cbe6d06221fb5168f0
Daniel Ramos <danielramosacosta1@gmail.com>
2024-06-16 11:21:14 +0200

    green: all tests passing

--- src/main/java/org/example/RomanNumerals.java
+++ src/main/java/org/example/RomanNumerals.java
@@ -3,8 +3,8 @@ package org.example;
 public class RomanNumerals {
 
   public static String toRoman(final int number) {
-    if (number == 1000) {
-      return "M";
+    if (number >= 1000) {
+      return "M" + toRoman(number - 1000);
     }
 
     if (number >= 900) {
