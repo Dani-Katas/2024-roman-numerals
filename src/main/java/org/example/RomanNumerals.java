481144609291e11e27d92244f663439293ef61e5
Daniel Ramos <danielramosacosta1@gmail.com>
2024-06-16 19:53:06 +0200

    inline romans

--- src/main/java/org/example/RomanNumerals.java
+++ src/main/java/org/example/RomanNumerals.java
@@ -1,12 +1,6 @@
 package org.example;
 
-import static org.example.Roman.UNITS;
-
-import java.util.List;
-
 public class RomanNumerals {
-
-  static List<Roman> romans = UNITS;
 
   private static int getRestableFor(final int i) {
     if (i == 1000) {
@@ -31,8 +25,8 @@ public class RomanNumerals {
   }
 
   public static String toRoman(final int number) {
-    for (int i = 0; i < romans.size() - 1; i++) {
-      final String roman1000 = temporalMethod(romans.get(i), number);
+    for (int i = 0; i < Roman.UNITS.size() - 1; i++) {
+      final String roman1000 = temporalMethod(Roman.UNITS.get(i), number);
       if (roman1000 != null) {
         return roman1000;
       }
