65879ef9ae21cc247a417490ff1c5eaec1ee78b7
Daniel Ramos <danielramosacosta1@gmail.com>
2024-06-16 18:48:51 +0200

    extract method with common code

--- src/main/java/org/example/RomanNumerals.java
+++ src/main/java/org/example/RomanNumerals.java
@@ -37,7 +37,41 @@ public class RomanNumerals {
   }
 
   public static String toRoman(final int number) {
-    final Roman roman1000 = romans.get(0);
+    final String roman1000 = temporalMethod(0, number);
+    if (roman1000 != null) {
+      return roman1000;
+    }
+
+    final String roman500 = temporalMethod(1, number);
+    if (roman500 != null) {
+      return roman500;
+    }
+
+    final String roman100 = temporalMethod(2, number);
+    if (roman100 != null) {
+      return roman100;
+    }
+
+    final String roman50 = temporalMethod(3, number);
+    if (roman50 != null) {
+      return roman50;
+    }
+
+    final String roman10 = temporalMethod(4, number);
+    if (roman10 != null) {
+      return roman10;
+    }
+
+    final String roman5 = temporalMethod(5, number);
+    if (roman5 != null) {
+      return roman5;
+    }
+
+    return "I".repeat(number);
+  }
+
+  private static String temporalMethod(final int index, final int number) {
+    final Roman roman1000 = romans.get(index);
     final int n1000 = roman1000.value();
     if (number >= n1000) {
       return roman1000.symbol() + toRoman(number - n1000);
@@ -47,62 +81,6 @@ public class RomanNumerals {
     if (number >= (n1000 - value1000)) {
       return toRoman(value1000) + toRoman(n1000) + toRoman(number - (n1000 - value1000));
     }
-
-    final Roman roman500 = romans.get(1);
-    final int n500 = roman500.value();
-    if (number >= n500) {
-      return roman500.symbol() + toRoman(number - n500);
-    }
-
-    int value500 = getRestableFor(n500);
-    if (number >= (n500 - value500)) {
-      return toRoman(value500) + toRoman(n500) + toRoman(number - (n500 - value500));
-    }
-
-    final Roman roman100 = romans.get(2);
-    final int n100 = roman100.value();
-    if (number >= n100) {
-      return roman100.symbol() + toRoman(number - n100);
-    }
-
-    int value100 = getRestableFor(n100);
-    if (number >= (n100 - value100)) {
-      return toRoman(value100) + toRoman(n100) + toRoman(number - (n100 - value100));
-    }
-
-    final Roman roman50 = romans.get(3);
-    final int n50 = roman50.value();
-    if (number >= n50) {
-      return roman50.symbol() + toRoman(number - n50);
-    }
-
-    int value50 = getRestableFor(n50);
-    if (number >= (n50 - value50)) {
-      return toRoman(value50) + toRoman(n50) + toRoman(number - (n50 - value50));
-    }
-
-    final Roman roman10 = romans.get(4);
-    final int n10 = roman10.value();
-    if (number >= n10) {
-      return roman10.symbol() + toRoman(number - n10);
-    }
-
-    int value10 = getRestableFor(n10);
-    if (number == (n10 - value10)) {
-      return toRoman(value10) + toRoman(n10) + toRoman(number - (n10 - value10));
-    }
-
-    final Roman roman5 = romans.get(5);
-    final int n5 = roman5.value();
-    if (number >= n5) {
-      return roman5.symbol() + toRoman(number - n5);
-    }
-
-    int value5 = getRestableFor(n5);
-    if (number == (n5 - value5)) {
-      return toRoman(value5) + toRoman(n5) + toRoman(number - (n5 - value5));
-    }
-
-    return "I".repeat(number);
+    return null;
   }
 }
