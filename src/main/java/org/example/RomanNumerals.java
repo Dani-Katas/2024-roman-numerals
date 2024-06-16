f4d50cc8a17f5fe436b0957103072e95898a8e95
Daniel Ramos <danielramosacosta1@gmail.com>
2024-06-16 18:13:07 +0200

    align to see the common pattern

--- src/main/java/org/example/RomanNumerals.java
+++ src/main/java/org/example/RomanNumerals.java
@@ -7,48 +7,48 @@ public class RomanNumerals {
       return "M" + toRoman(number - 1000);
     }
 
-    if (number >= 900) {
-      return toRoman(100) + toRoman(1000) + toRoman(number - 900);
+    if (number >= (1000 - 100)) {
+      return toRoman(100) + toRoman(1000) + toRoman(number - (1000 - 100));
     }
 
-    if(number >= 500) {
+    if (number >= 500) {
       return "D" + toRoman(number - 500);
     }
 
-    if(number >= 400) {
-      return toRoman(100) + toRoman(500) + toRoman(number - 400);
+    if (number >= (500 - 100)) {
+      return toRoman(100) + toRoman(500) + toRoman(number - (500 - 100));
     }
 
-    if(number >= 100) {
+    if (number >= 100) {
       return "C" + toRoman(number - 100);
     }
 
-    if(number >= 90) {
-      return toRoman(10) + toRoman(100) + toRoman(number - 90);
+    if (number >= (100 - 10)) {
+      return toRoman(10) + toRoman(100) + toRoman(number - (100 - 10));
     }
 
-    if(number >= 50) {
+    if (number >= 50) {
       return "L" + toRoman(number - 50);
     }
 
-    if(number >= 40) {
-      return toRoman(10) + toRoman(50) + toRoman(number - 40);
+    if (number >= (50 - 10)) {
+      return toRoman(10) + toRoman(50) + toRoman(number - (50 - 10));
     }
 
     if (number >= 10) {
       return "X" + toRoman(number - 10);
     }
 
-    if(number == 9) {
-      return toRoman(1) + toRoman(10);
+    if (number == (10 - 1)) {
+      return toRoman(1) + toRoman(10) + toRoman(number - (10 - 1));
     }
 
     if (number >= 5) {
       return "V" + toRoman(number - 5);
     }
 
-    if (number == 4) {
-      return toRoman(1) + toRoman(5);
+    if (number == (5 - 1)) {
+      return toRoman(1) + toRoman(5) + toRoman(number - (5 - 1));
     }
 
     return "I".repeat(number);
